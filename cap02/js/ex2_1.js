61f43f2d335d3b98e9a38dfa6e0a14c9a4fb15fd
iago <iagopacheco01@icloud.com>
2024-07-20 14:49:56 -0300

    cap02-ex02_1

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,14 @@
+// cria uma referência ao form e ao elemento h3 (onde séra exibida a reposta)
+
+const frm = window.document.querySelector("form")
+const resp = window.document.querySelector("h3")
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+
+frm.addEventListener("submit", (e) => {
+  const nome = frm.inNome.value  // obtém o nome digitado no form
+  resp.innerText = `Olá, ${nome}!` // exibe a resposta do programa
+  e.preventDefault() // evita o envio do form
+
+}
+)
